4452b6cb6ead7d2087e8c0ddbe07f9d234b900f9
dkris <crismajesus82@gmail.com>
2025-07-23 21:58:12 -0500

    Cambio de prueba por Cris

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <p>Hola mundo</p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}

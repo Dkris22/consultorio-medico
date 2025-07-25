4709ca82c13088b1d5919895568ef982ef529351
dkris <crismajesus82@gmail.com>
2025-07-24 19:49:17 -0500

    Cambios para que se trabaje en Typescript

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to the Consultorio Médico</h1>
+      {/* Other components and content can be added here */}
+    </div>
+  );
+}
+export default App;
